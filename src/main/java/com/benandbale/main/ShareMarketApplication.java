598c877f617397010c43eafdd8ae493eae4b4342
Kapil Soni <kapil.soni@decurtis.com>
2019-05-28 21:53:32 +0530

    Initiaizing project.

---
+++ src/main/java/com/benandbale/main/ShareMarketApplication.java
@@ -0,0 +1,17 @@
+package com.benandbale.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ShareMarketApplication extends Application {
+	
+	public static void main(String ...args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+	}
+
+}
